a5f40b0af11458a98db042d087ad5fa3fae57b8a
NinJa427 <17398081021@163.com>
2020-10-11 22:43:50 +0800

    登录表单

---
+++ src/utils/validator.ts
@@ -0,0 +1,42 @@
+/**
+ *
+ * @export
+ * @description 去无效字符
+ * @name stripscript
+ * @param {string} str
+ * @return {string}
+ */
+export function stripscript(str: string): string {
+  let pattern = new RegExp(
+    "[`~!@#$^&*()=|{}':;',\\[\\]%<>/?~！@#￥……&*（）&;—|{ }【】‘；：”“'。，、？]"
+  );
+  let rs = "";
+  for (let i = 0; i < str.length; i++) {
+    rs = rs + str.substr(i, 1).replace(pattern, "");
+  }
+  return rs;
+}
+/**
+ * @export
+ * @description 验证用户名/邮箱
+ * @name valUsername
+ * @param {string} value
+ * @returns {boolean}
+ */
+export function valUsername(value: string): boolean {
+    console.log(value)
+  let reg = /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+  return !reg.test(value) ? true : false;
+}
+
+/**
+ * @export
+ * @description 验证密码
+ * @name valPassword
+ * @param {string} value
+ * @returns {boolean}
+ */
+export function valPassword(value: string): boolean {
+  let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+  return !reg.test(value) ? true : false;
+}
--- src/views/Login/components/login.vue
+++ src/views/Login/components/login.vue
@@ -9,15 +9,17 @@
   <div class="login-component">
     <a-form
       class="form"
+      ref="formData"
       :model="formData"
-      :label-col="labelCol"
-      :wrapper-col="wrapperCol"
+      :rules="rules"
+      :label-col="formStyle.labelCol"
+      :wrapper-col="formStyle.wrapperCol"
     >
-      <a-form-item label="用户名">
-        <a-input v-model:value="formData.user" placeholder="Username">
+      <a-form-item required has-feedback label="用户名" name="user">
+        <a-input v-model:value="formData.user" placeholder="Username" autocomplete="off">
         </a-input>
       </a-form-item>
-      <a-form-item label="密码">
+      <a-form-item required has-feedback label="密码" name="password" autocomplete="off">
         <a-input
           v-model:value="formData.password"
           type="password"
@@ -26,32 +28,70 @@
         </a-input>
       </a-form-item>
     </a-form>
+    <button @change="changeV"></button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, watch, watchEffect } from "vue";
+import { stripscript, valUsername, valPassword } from "@/utils/validator.ts";
 interface loginForm {
-  user?: string;
-  password?: string;
+  user: string | undefined;
+  password: string | undefined;
+}
+interface ruleFn {
+  (rule: object, value: string, callback: () => any): any;
 }
 export default defineComponent({
   setup() {
-    const labelCol = { span: 6 }
-    const wrapperCol = { span: 16 }
-    const formData = reactive<loginForm>({
-      user: "",
+    // 表单样式
+    const formStyle = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 16 },
+    };
+    // 表单数据
+    const formData:loginForm = reactive({
+      user: "17398081021@163.com",
       password: "",
     });
+    // 表单验证
+    // 自定义验证规则
+    let validateUser: ruleFn = async (rule, value, callback) => {
+      if (!value) {
+        return Promise.reject("用户名不能为空");
+      } else {
+        if (valUsername(value)) {
+          return Promise.reject("用户名格式不正确");
+        } else {
+          return Promise.resolve();
+        }
+      }
+    };
+    let validatePassword: ruleFn = async (rule, value, callback) => {
+      formData.password = stripscript(value);
+      value = formData.password;
+      if (!value) {
+        return Promise.reject("密码不能为空");
+      } else if (valPassword(value)) {
+        return Promise.reject("请输入6-20位数字+字母");
+      } else {
+        return Promise.resolve();
+      }
+    };
+    const rules = reactive({
+      userRule: [{ validator: validateUser, trigger: "change" }],
+      passwordRule: [{ validator: validatePassword, trigger: "change" }],
+    });
+    watchEffect(()=>console.log(formData.user))
     return {
-      labelCol,
-      wrapperCol,
+      formStyle,
       formData,
+      rules
     };
   },
 });
 </script>
 
 <style scoped lang="scss">
-@import './login.scss';
+@import "./login.scss";
 </style>
