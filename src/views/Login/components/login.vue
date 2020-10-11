e84a02531e66afbf4a2ffb0d0f641f06e1115b30
NinJa427 <17398081021@163.com>
2020-10-11 23:41:50 +0800

    表单验证

--- src/views/Login/components/login.vue
+++ src/views/Login/components/login.vue
@@ -9,17 +9,25 @@
   <div class="login-component">
     <a-form
       class="form"
-      ref="formData"
-      :model="formData"
-      :rules="rules"
       :label-col="formStyle.labelCol"
       :wrapper-col="formStyle.wrapperCol"
     >
-      <a-form-item required has-feedback label="用户名" name="user">
-        <a-input v-model:value="formData.user" placeholder="Username" autocomplete="off">
+      <a-form-item
+        label="用户名"
+        v-bind="validateInfos.user"
+      >
+        <a-input
+          v-model:value="formData.user"
+          placeholder="Username"
+          autocomplete="off"
+        >
         </a-input>
       </a-form-item>
-      <a-form-item required has-feedback label="密码" name="password" autocomplete="off">
+      <a-form-item
+        label="密码"
+        v-bind="validateInfos.password"
+        autocomplete="off"
+      >
         <a-input
           v-model:value="formData.password"
           type="password"
@@ -28,13 +36,13 @@
         </a-input>
       </a-form-item>
     </a-form>
-    <button @change="changeV"></button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, watch, watchEffect } from "vue";
 import { stripscript, valUsername, valPassword } from "@/utils/validator.ts";
+import { useForm } from "@ant-design-vue/use";
 interface loginForm {
   user: string | undefined;
   password: string | undefined;
@@ -43,50 +51,37 @@ interface ruleFn {
   (rule: object, value: string, callback: () => any): any;
 }
 export default defineComponent({
-  setup() {
+  setup(props, ctx) {
     // 表单样式
     const formStyle = {
       labelCol: { span: 6 },
       wrapperCol: { span: 16 },
     };
     // 表单数据
-    const formData:loginForm = reactive({
-      user: "17398081021@163.com",
+    const formData = reactive({
+      user: "",
       password: "",
     });
-    // 表单验证
-    // 自定义验证规则
-    let validateUser: ruleFn = async (rule, value, callback) => {
-      if (!value) {
-        return Promise.reject("用户名不能为空");
-      } else {
-        if (valUsername(value)) {
-          return Promise.reject("用户名格式不正确");
-        } else {
-          return Promise.resolve();
-        }
-      }
-    };
-    let validatePassword: ruleFn = async (rule, value, callback) => {
-      formData.password = stripscript(value);
-      value = formData.password;
-      if (!value) {
-        return Promise.reject("密码不能为空");
-      } else if (valPassword(value)) {
-        return Promise.reject("请输入6-20位数字+字母");
-      } else {
-        return Promise.resolve();
-      }
-    };
     const rules = reactive({
-      userRule: [{ validator: validateUser, trigger: "change" }],
-      passwordRule: [{ validator: validatePassword, trigger: "change" }],
+      user: [
+        {
+          required: true,
+          message: "Please input name",
+        },
+      ],
+      password: [
+        {
+          required: true,
+          message: "Please input name",
+        },
+      ],
     });
-    watchEffect(()=>console.log(formData.user))
+    const { resetFields, validate, validateInfos } = useForm(formData, rules);
     return {
+      validateInfos,
       formStyle,
       formData,
-      rules
+      rules,
     };
   },
 });
