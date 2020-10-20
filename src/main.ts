ed8a59dd8f49a566789cefb9e1fc8c7424c4e496
yq <17398081021@163.com>
2020-10-20 19:05:12 +0800

    引入markdown-it 解析md文件

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueMarkdownIt from 'vue3-markdown-it';
 // 引入全局样式
-import 'ant-design-vue/dist/antd.css';
-import './assets/css/main.scss'
+import 'highlight.js/styles/monokai.css';
+import "ant-design-vue/dist/antd.css";
+import "./assets/css/main.scss";
 // 引入antd
-import Antd from 'ant-design-vue';
-const app = createApp(App)
-app.use(store).use(router).use(Antd).mount('#app')
-
-
+import Antd from "ant-design-vue";
+const app = createApp(App);
+app
+  .use(store)
+  .use(router)
+  .use(Antd)
+  .use(VueMarkdownIt)
+  .mount("#app");
--- src/views/Admin/AddArticle/index.vue
+++ src/views/Admin/AddArticle/index.vue
@@ -4,15 +4,108 @@
 * @date 2020/10/20 16:20:44
 !-->
 <template>
-  <div>AddArticle</div>
+  <div class="addArticle-wrap">
+    <div class="form">
+      <a-form :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+        <a-form-item label="标题">
+          <a-input v-model:value="form.title" />
+        </a-form-item>
+        <a-form-item label="分类">
+          <a-select v-model:value="form.category" placeholder="请选择分类">
+            <a-select-option
+              :value="item.value"
+              v-for="(item, index) in categories"
+              :key="index"
+            >
+              {{ item.label }}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item label="技术栈">
+          123
+        </a-form-item>
+        <a-form-item label="选择日期">
+          <a-date-picker
+            v-model:value="form.date"
+            show-time
+            type="date"
+            placeholder="Pick a date"
+            style="width: 100%;"
+          />
+        </a-form-item>
+        <a-form-item label="上传MD文件">
+          <a-upload
+            v-model:fileList="fileList"
+            name="file"
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            :headers="headers"
+            @change="handleChange"
+          >
+            <a-button> <upload-outlined /> Click to Upload </a-button>
+          </a-upload>
+        </a-form-item>
+      </a-form>
+    </div>
+    <!-- <vue3-markdown-it :source='source' /> -->
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from "vue";
+import { Article } from "@/types/article";
+import { UploadOutlined } from "@ant-design/icons-vue";
+import { message } from "ant-design-vue";
 export default defineComponent({
   name: "AddArticle",
+  components: {
+    UploadOutlined,
+  },
   setup() {
-    return {};
+    const form: Article = reactive({
+      title: "1",
+      category: undefined,
+      date: undefined,
+    });
+    // 文章分类 根据接口获取
+    const categories = [
+      {
+        value: "js",
+        label: "js",
+      },
+      {
+        value: "webpack",
+        label: "webpack",
+      },
+      {
+        value: "html/css",
+        label: "html/css",
+      },
+    ];
+    const source = ref("### 123");
+    const onSubmit = () => {};
+    const handleChange = (info: any) => {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    };
+    return {
+      categories,
+      source,
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      other: "",
+      form,
+      fileList: [],
+      headers: {
+        authorization: "authorization-text",
+      },
+      handleChange
+    };
   },
 });
 </script>
